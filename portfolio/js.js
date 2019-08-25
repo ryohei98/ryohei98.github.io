770089261bdca6461b27d72bd5970708f1748e96
Ryohei Kondo <konpei.to98@gmail.com>
2019-08-25 18:19:21 +0900

    update

--- portfolio/js.js
+++ portfolio/js.js
@@ -18,45 +18,114 @@ $(function() {
     }, 30000).animate({
       'width': '100%'
     }, 30000);
-  },);
+  }, );
+
+  let $current = undefined;
 
   //menuを出す
   $('#wrapper-right').delay(2200).slideDown('slow');
 
   //aboutのクリック挙動
   $('#about-btn').click(function() {
-    $(this).addClass('current');
-    $('#about-btn:hover').css('text-decoration','none')
-    $(this).css('position', 'absolute').css('cursor','default');
+    if ($(this).hasClass('current')) {
 
-    //topの移動
-    $('#top').delay(300).animate({
-      'width': '10vw',
-      'height': '10vw',
-      'opacity': '1'
-    }, 700);
-    $('#inner').delay(300).animate({
-      'top': '0.4vw',
-      'width': '10vw'
-    });
-    $('#top-name').delay(300).animate({
-      'fontSize': '2vw',
-    }, 650);
-    $('#top-pf').fadeOut('slow');
+    } else if ($current === $('#archvies-btn')) {
+      $(this).addClass('current');
+      $('#archives-btn').removeClass('current');
 
-    //about-btnの移動
+      //aboutをリストから消しタイトルに、archivesをリストに追加
+      $('#title-archives').fadeOut('slow');
+      $('#title-about').fadeIn('slow');
 
-    $(this).delay(300).animate({
-      'top': '10vw'
-    }, 700).delay(300).animate({
-      'left': '20vw'
-    }, 700).delay(300).animate({
-      'fontSize': '5vw',
-      'fontWeight': 'normal'
-    }, 700)
+      //content-aboutの出現
+      $('#content-archives').fadeOut('slow');
+      $('#content-about').fadeIn('slow');
 
-    //contentの出現
+      $current = $('#about-btn');
+      console.log($current);
 
-    $('#content-about').delay(300).fadeIn();
-  })
+    } else {
+      $(this).addClass('current');
+      $('#archives-btn').removeClass('current');
+
+      //topの移動
+      $('#top').delay(300).animate({
+        'width': '10vw',
+        'height': '10vw',
+        'opacity': '1'
+      }, 700);
+      $('#inner').delay(300).animate({
+        'top': '0.4vw',
+        'width': '10vw'
+      });
+      $('#top-name').delay(300).animate({
+        'fontSize': '2vw',
+      }, 650);
+      $('#top-pf').fadeOut('slow');
+
+      //about-btnの移動
+
+      $('#title-archives').delay(1000).fadeOut('slow');
+      $('#title-about').delay(1200).fadeIn('slow');
+
+      //contentの出現
+      $('#content-archives').delay(1000).fadeOut('slow');
+      $('#content-about').delay(1300).fadeIn();
+      $current = $('#about-btn');
+      console.log($current);
+    };
+  });
+
+
+  $('#archives-btn').click(function() {
+    if ($(this).hasClass('current')) {
+
+    } else if ($current === $('#about-btn')) {
+      $(this).addClass('current');
+      $('#archives-btn').removeClass('current');
+
+      //archivesのバックグラウンドカラーを変更、archivesをリストに追加
+      $(this).css('background-color', '#999');
+      $('#title-archives').fadeIn('slow');
+      $('#title-about').fadeOut('slow');
+
+      //content-archivesの出現
+      $('#content-about').fadeOut('slow');
+      $('#content-archives').fadeIn('slow');
+      $current = $('#archives-btn');
+      console.log($current);
+
+    } else {
+      $(this).addClass('current');
+      $('#about-btn').removeClass('current');
+
+      //topの移動
+      $('#top').delay(300).animate({
+        'width': '10vw',
+        'height': '10vw',
+        'opacity': '1'
+      }, 700);
+      $('#inner').delay(300).animate({
+        'top': '0.4vw',
+        'width': '10vw'
+      });
+      $('#top-name').delay(300).animate({
+        'fontSize': '2vw',
+      }, 650);
+      $('#top-pf').fadeOut('slow');
+
+      //about-btnの移動
+      $('#title-about').delay(1000).fadeOut('slow');
+      $('#title-archives').delay(1200).fadeIn('slow');
+
+
+      //contentの出現
+      $('#content-about').delay(1000).fadeOut('slow');
+      $('#content-archives').delay(1300).fadeIn();
+      $current = $('#archives-btn');
+      console.log($current);
+    };
+  });
+
+
 });
