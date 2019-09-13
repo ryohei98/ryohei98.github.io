ffe82a3ce5ba9eb66f31de33bcd6e4b4020a991c
Ryohei Kondo <konpei.to98@gmail.com>
2019-09-13 10:46:17 +0900

    update

--- eisai-web/js.js
+++ eisai-web/js.js
@@ -1,13 +1,33 @@
-$(function(){
-  $('#menu-icon').click(function(){
-      $('#mobile-menu').slideToggle();
-      $('#menu-icon').fadeOut(300);
-      $('#menu-close-icon').delay(300).fadeIn(300);
-    });
+$(function() {
+  $('#menu-icon').click(function() {
+    $('#mobile-menu').slideToggle();
+    $('#menu-icon').fadeOut(300);
+    $('#menu-close-icon').delay(300).fadeIn(300);
+  });
 
-  $('#menu-close-icon').click(function(){
+  $('#menu-close-icon').click(function() {
     $('#mobile-menu').slideToggle();
     $('#menu-close-icon').fadeOut(300);
     $('#menu-icon').delay(300).fadeIn(300);
   });
+
+  if ($('header').width() > 768) {
+
+    let $win = $(window),
+      $main = $('.container'),
+      $nav = $('.side-bar'),
+      navHeight = $nav.outerHeight(),
+      navPos = $nav.offset().top,
+      fixedClass = 'is-fixed';
+
+    $win.on('load scroll', function() {
+        let value = $(this).scrollTop();
+        if (value > navPos - 83) {
+          $nav.addClass(fixedClass);
+        } else {
+          $nav.removeClass(fixedClass);
+          $main.css('margin-top', '0');
+        }
+      });
+    }
 })
